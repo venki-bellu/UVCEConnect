b79c0da451c295205fc13f56752f9e95d95c856a
venkibellu <venkateshrbelavadi@gmail.com>
2017-07-12 19:28:12 +0530

    Initial commit

---
+++ app/src/main/java/com/venkibellu/uvceconnect/LogInPage.java
@@ -0,0 +1,65 @@
+package com.venkibellu.uvceconnect;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import javax.net.ssl.SSLSessionContext;
+
+public class LogInPage extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    Intent homepageIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_page);
+
+
+        homepageIntent = new Intent(this, HomePage.class);
+        loginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        callbackManager = CallbackManager.Factory.create();
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(homepageIntent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Facebook LogIn Cancelled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
+}
