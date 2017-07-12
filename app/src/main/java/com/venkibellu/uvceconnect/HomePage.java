0916646aa753260a7cc249232439d19d6375482f
CaptainDaVinci <yashkothari199767@gmail.com>
2017-07-12 21:06:03 +0530

    Test Commit

--- app/src/main/java/com/venkibellu/uvceconnect/HomePage.java
+++ app/src/main/java/com/venkibellu/uvceconnect/HomePage.java
@@ -1,13 +1,13 @@
 package com.venkibellu.uvceconnect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
+
 }
